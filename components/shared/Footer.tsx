14966a583912898237c1bd9cf277bb3d0a453d95
kaustubh6220 <147793155+kaustubh6220@users.noreply.github.com>
2024-03-16 15:28:24 +0530

    Update Footer.tsx

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,11 +6,16 @@ const Footer = () => {
   return (
     <footer className='border-t'>
       <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
-        <Link href='/'>
-          <Image src='/assets/images/logo.svg' alt='logo' width={128} height={38}></Image>
-        </Link>
-
-        <p>MIT ADT University</p>
+        <div className="flex items-center gap-2">
+          <Link href='/'>
+            <Image src='/assets/images/mitlogo.jpeg' alt='MIT ADT University' width={58} height={12}></Image>
+          </Link>
+          <p>MIT ADT University</p>
+        </div>
+        
+        <div>
+          <p>&copy; {new Date().getFullYear()} MIT ADT University. All Rights Reserved.</p>
+        </div>
       </div>
     </footer>
   )
